84c3794b9cc3974d1883efa711c30099e42e2710
Felicia Fong <feliciasfong@gmail.com>
2016-10-18 22:27:28 -0700

    text onchange event handler

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -7,19 +7,31 @@ class App extends React.Component {
 
     this.state = {
       username: '',
+      newTweet: { user: '', text: '' },
       tweets: [
         { user: 'felicia', text: 'testing' },
         { user: 'dan',     text: 'testing testing' },
         { user: 'felicia', text: '3rd message' }
       ]
     };
+
+    this.onNewTextChange = this.onNewTextChange.bind(this);
+  }
+
+  onNewTextChange(event) {
+    this.setState({
+      newTweet: {
+        user: this.state.newTweet.user,
+        text: event.target.value
+      }
+    });
   }
 
   render() {
     return (
       <div>
-        <input type="text" value="Username" />
-        <input type="text" value="Type Here" />
+        <input type="text" value="" />
+        <input type="text" value={this.state.newTweet.text} onChange={this.onNewTextChange} />
         <button>Submit</button>
         <div>
           <Feed tweets={this.state.tweets} username={this.state.username} />
